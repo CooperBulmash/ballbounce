3adeae3b8dc059a331730d148f749c84418a31c5
Cooper B <cooperbulmash@gmail.com>
2015-05-31 21:19:19 +0000

    Fixing File Locations

--- platforms/android/assets/www/js/dsound.js
+++ platforms/android/assets/www/js/dsound.js
@@ -1,7 +1,7 @@
 document.querySelector("#button1").addEventListener("touchend", playMP3, false);
 
 function playMP3() {
-    var mp3URL = "/android_asset/www/sounds/santa.mp3";
+    var mp3URL = "/android_asset/www/sounds/fartnoise.mp3";
     try{
         var media = new Media(mp3URL, null, mediaError);
     } catch(e) {
@@ -22,13 +22,40 @@ var showy = document.querySelector("#showy");
 var showz = document.querySelector("#showz");
 var showt = document.querySelector("#showt");
 
-function onSuccess(acceleration) {
+
+function showAccel(acceleration) {
     showx.innerText = acceleration.x.toString();
     showy.innerText = acceleration.y.toString();
     showz.innerText = acceleration.z.toString();
     showt.innerText = acceleration.timestamp.toString();
 };
 
-function onError() {
+
+function showAccelError() {
     alert('onError!');
 };
+
+var start = 0;
+
+function dsoundTick(timestamp) {
+  if (start === 0) {
+      start = timestamp;
+    }
+  var progress = timestamp - start;
+  try {
+        navigator.accelerometer.getCurrentAcceleration(showAccel, showAccelError);
+      } catch(e) {
+          //first time it'll fail
+          //but this way it doesn't kill the prog.
+      }
+
+  if (progress < 120000) {
+    window.requestAnimationFrame(dsoundTick);
+  }
+}
+
+try{
+    window.requestAnimationFrame(dsoundTick);
+} catch(e){
+    alert('reqanimframe not working: ' + e.message);
+}
